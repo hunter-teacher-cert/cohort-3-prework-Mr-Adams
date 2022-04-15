f7e4fa7bed0c8e12751d6dfbece866097833316f
Mr-Adams <66141358+Mr-Adams@users.noreply.github.com>
2022-04-15 04:09:09 +0000

    Added Power method

--- Pre06/Loops.java
+++ Pre06/Loops.java
@@ -7,6 +7,7 @@ public class Loops {
   public static void main(String[] args) {
     System.out.println(squareroot(7.0));
     System.out.println(power(3.0,3));
+    System.out.println(factorial(4));
     }
   
   public static double squareroot(double a) {
@@ -19,13 +20,25 @@ public class Loops {
       } while (difference < .0001); 
         return newguess;
     }
-   public static double power(double x, int n) {
+  public static double power(double x, int n) {
      double answer = 1.0;
      for (int i=0; i<n; i++){
        answer *= x; 
      }
      return answer;
   }  
-  
-}     
+  public static int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    int result = 1;
+    for(int i = 1; i<=n;i++){
+      result = result * i;
+    }
+    return result;
+}
+    
+
+   
+}  
      
